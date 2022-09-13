dbee2055032387703e9e03a2c1bd323a62d41efa
Yumi Wang <w67238@outlook.com>
2022-09-13 22:02:53 +1200

    update eq and js blogs

---
+++ styles/emotional-intelligence.js
@@ -0,0 +1,13 @@
+document.getElementById("heading1").innerHTML = "What is Emotional Intelligence?"
+document.getElementById("para1").innerHTML = "Emotional intelligence (emotional quotient or EQ), in short, is the ability to be aware, understand, and regulate the emotions oneself. Also, to notice and understand the feelings of others."
+document.getElementById("para2").innerHTML = "Five components of EQ are:"
+
+
+document.getElementById("heading2").innerHTML = "How is it different to IQ?"
+document.getElementById("para3").innerHTML = "IQ focuses on people's intelligence, critical thinking, problem-solving, calculation ability, logical thinking, etc. While EQ is more of the ability to feel and understand the emotions of oneself and others."
+
+document.getElementById("heading3").innerHTML = "Why is it important?"
+document.getElementById("para4").innerHTML = "Emotional intelligence can reduce the impact of negative feelings and improve interpersonal relationships. Life is hard, and things don't always go your way. EQ helps us to cope with negative emotions and provides the energy to overcome difficulties in life."
+document.getElementById("para5").innerHTML = "Knowing and caring for people's feelings and emotions is also essential to build and maintaining relationships with others. EQ is a critical human skill to maintain healthy relationships with people in our lives."
+
+
---
+++ styles/javascript-dom.js
@@ -0,0 +1,19 @@
+document.getElementById("heading1").innerHTML = "JavaScript and its relationship to HTML and CSS"
+document.getElementById("para1").innerHTML = "HTML (Hypertext Markup Language) and CSS (Cascading Style Sheets) are two critical technologies for building Web pages. HTML provides the structure of the page, and CSS is in charge of the style and layout. Think HTML is like kitchen appliances sketch, simple, black and white. CSS can decorate the drawing, making it colourful and delightful. But at this point, the appliances are still pictures without actual functioning."
+document.getElementById("para2").innerHTML = "JavaScript is the programming language of the Web. It can help to enhance the user experience interacting with web pages. It determines what happens when users click, hover or type within certain elements. JavaScript brings functions to kitchen appliances. Rice cookers can make rice, blenders can make smoothies, and jugs can boil water, etc."
+
+
+document.getElementById("heading2").innerHTML = "Control flow and Loops"
+document.getElementById("para3").innerHTML = "Control flow is like going to school. Generally speaking, students need to start from year 1 to year 13 instead of going to year 6 first and then jumping back to year 2. Decision-making will also be involved in terms of choosing subjects as it will lead you on different paths. "
+document.getElementById("para4").innerHTML = "Loops are used to run repeated processes until a certain criteria is met. For instance, students will need to pass all the exams to graduate. If a person has one or more failed course(s), he will need to take it/them over and over again until passing all of the tests to be able to graduate. "
+
+document.getElementById("heading3").innerHTML = "The DOM"
+document.getElementById("para5").innerHTML = "The DOM (Document Object Model) is an application programming interface for web documents. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page. "
+document.getElementById("para6").innerHTML = "Elements in web documents can be pictured as family trees. DOM is the tool to get access to the elements by their names. Just like home apps allows you to remote control your home appliances. Locating the appliance by its name and then you can turn it on, change its descriptions,  and make you a cup of coffee. "
+
+document.getElementById("heading4").innerHTML = "Arrays and Objects"
+document.getElementById("para7").innerHTML = "Arrays are used to set up a list of items with order. And the items are within the same category. Say we have an array called 'fruit name' in the array, we have coconut, watermelon, and peach. The fruits are stored in a numbered order, and the index starts from 0. So if we want to call out coconut, we use the fruit name[0]. Fruit name[2] will be peach in the same manner."
+document.getElementById("para8").innerHTML = "Objects are like business cards. We can store different categories of data in each object. For example, we have an object/business card called coconut. In the object/business card, the name is coconut. Hometown: Indo-Malaya, Genus: Cocos, and Species: Nucifera. We can use strings/names to look for information stored in the object instead of the index number."
+
+document.getElementById("heading5").innerHTML = "Functions"
+document.getElementById("para9").innerHTML = "Function is a block of code that can be used over and over again. Imagine you are working in a food box factory line. You are asked to put ingredients 1, 2, and 5 in a box when people order meal A. Ingredients 2, 6, 7, and 9 for meal B. It is tiring and easy to make mistakes. So you decide to program a robot to work for you. When the robot sees the meal's name, it automatically goes to the ingredients locations accordingly and takes one each. Now you can just sit back, have a cup of tea, and watch the robot work for you."
