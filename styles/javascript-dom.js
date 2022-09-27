4053e8808f766421c627abcfc35cf8c496d2b0b4
Yumi Wang <w67238@outlook.com>
2022-09-27 22:18:25 +1300

    add img

--- styles/javascript-dom.js
+++ styles/javascript-dom.js
@@ -15,30 +15,28 @@ document.getElementById('para2-3').innerHTML =
   'The example below shows how the computer executes the code from top to bottom, printing the sentences one by one, and each statement will start in a new line.'
 document.getElementById('para2-4').innerHTML =
   'Loops are used to run repeated processes until a certain criteria is met. For instance, students will need to pass all the exams to graduate. If a person has one or more failed course(s), he/she will need to take it/them over and over again until passing all of the tests to be able to graduate. '
-document.getElementById(
-  'para2-5'
-).innerHTML = `The following example will explain how loop works. Inside the bracket after the while key work, is the condition. If the student_score doesn't meet the condition, the while loop will go on again until student_score is equal to or more than 50.`
+document.getElementById('para2-5').innerHTML =
+  `The following example will explain how loop works. Inside the bracket after the while key work, is the condition. If the student_score doesn't meet the condition, the while loop will go on again until student_score is equal to or more than 50.`
 
 document.getElementById('heading3').innerHTML = 'The DOM'
 document.getElementById('para3-1').innerHTML =
   'The DOM (Document Object Model) is an application programming interface (API) for web documents. When a web page is loaded, the browser creates a Document Object Model of the page. By DOM manipulation we can add, change and remove any HTML elements or attributes and CSS styles associated with it.'
 document.getElementById('para3-2').innerHTML =
   'But how exactly does DOM manipulate the elements? Well, elements in web documents can be pictured as family trees. If you have not already known, a basic HTML structure looks like this:'
-document.getElementById('para3-3').innerHTML =
-  'From the perspective of DOM, it sees web documents this way'
+document.getElementById(
+  'para3-3'
+).innerHTML = `From the perspective of DOM, it sees web documents as family trees. DOM can access the web documents' elements by their tags (as the example shows), ids, and classes, etc.`
 document.getElementById('para3-4').innerHTML =
   'We can manipulating the document-object from console. Open a webpage and press F12 to open the developer tools and go to console. The page will look like this:'
 document.getElementById('para3-5').innerHTML = `In the console, we type in 
   <br /> > <span id="text-highlight">document.querySelector('p').innerHTML = "This is how we manipulate DOM!"</span>
   <br />And then press enter. You can see the paragraph contents changed to what we put in the console.`
-document.getElementById('para3-6').innerHTML =  
+document.getElementById('para3-6').innerHTML =
   `If we go further into the command lines, the <span id="text-highlight">document</span> represents the Document Object. When you load an HTML document in a browser, it is converted into a document object. We can access the document object by typing document into the Console-tab as we did before. `
-document.getElementById('para3-7').innerHTML = 
+document.getElementById('para3-7').innerHTML =
   `The Document method <span id="text-highlight">querySelector('p')</span> returns the first Element within the HTML document that matches the specified selector, or group of selectors. As we only have one paragraph sector in the document, this paragraph will be returned.`
-document.getElementById('para3-8').innerHTML = 
-  `The <span id="text-highlight">innerHTML</span> is a property of the Element that allows you to get or set the HTML markup contained within the element. It can simply be understood as "changing the text in the selected sector". As we have already locked in the paragraph within the HTML document. To manipulate the content, we only need to assign the new text with <span id="text-highlight">= "This is how we manipulate DOM!"</span> `
-document.getElementById('para3-9').innerHTML = 
-  `If you want to try it out, changing the background color of the page. Open the developer tools and in the console, type in: 
+document.getElementById('para3-8').innerHTML = `The <span id="text-highlight">innerHTML</span> is a property of the Element that allows you to get or set the HTML markup contained within the element. It can simply be understood as "changing the text in the selected sector". As we have already locked in the paragraph within the HTML document. To manipulate the content, we only need to assign the new text with <span id="text-highlight">= "This is how we manipulate DOM!"</span> `
+document.getElementById('para3-9').innerHTML = `If you want to try it out, changing the background color of the page. Open the developer tools (F12) and in the console, type in: 
  <br />> <span id="text-highlight">document.body.style.backgroundColor = '#7D6F86'</span>. Now let see how it goes!`
 
 document.getElementById('heading4').innerHTML = 'Arrays and Objects'
@@ -49,7 +47,6 @@ document.getElementById('para4-3').innerHTML = `An object is another data type i
 document.getElementById('para4-4').innerHTML = `How about accessing properties on the Object? We can access the property values by their property names/keys. There two ways: dot notation and bracket notation:`
 document.getElementById('para4-5').innerHTML = `Remember to use quotation marks in the brackets when you are using the bracket notation. Otherwise, you will get an undefined error. However, if you assign a property name to a new variable, quotation marks are not needed as we are accessing the value via a variable name instead of a string. `
 document.getElementById('para4-6').innerHTML = `Another thing to note is if the property names are two separate words, we can only use bracket notation to access the value.`
-
 
 document.getElementById('heading5').innerHTML = 'Functions'
 document.getElementById('para5-1').innerHTML =
